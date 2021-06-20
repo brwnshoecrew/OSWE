0dfc5ba3758cbbba313b9e2adbf822914b4143c1
brwnshoecrew <54796086+brwnshoecrew@users.noreply.github.com>
2021-06-20 07:27:12 -0500

    Changed to functions.

--- canarytokens.py
+++ canarytokens.py
@@ -1,13 +1,10 @@
+#!/usr/bin/python3
+import os
 import requests
 from sys import argv
 
 
-
-
-
-if len(argv) == 2:
-    memo =str(argv[1])
-
+def generate_honeytoken(memo):
     multipart_form_data = {
         'type': 'web',
         'email': 'matthew.vorhees@gmail.com',
@@ -21,19 +18,25 @@ if len(argv) == 2:
         'sql_server_trigger_name': 'TRIGGER1',
         'redirect_url': '',
     }
-
+	
     session = requests.Session()
 
-    # Send an HTTP POST request an existing comment with the stored XSS payload to send the cookie of any user navigating to this page back to our server.
+	# Send an HTTP POST request an existing comment with the stored XSS payload to send the cookie of any user navigating to this page back to our server.
     response = requests.post('https://canarytokens.org/generate', files=multipart_form_data)
-
+	
     if str(response.status_code) == "200":
         data = response.json()
-        print("\nhttp://canarytokens.org/" + data['Token'] + "\n")
+        honeytoken = "http://canarytokens.org/" + str(data['Token'])
+        return honeytoken
     else:
         print("Didn't get a 200 response code...Exiting.")
         exit()
 
-else:
-    print('Please provide a memo argument.')
-    exit()
+if __name__ == "__main__":
+    if len(argv) == 2:
+        memo =str(argv[1])
+        honeytoken = generate_honeytoken(memo)
+        print("\n" + honeytoken + "\n")
+    else:
+        print("Please provide a memo argument.")
+        exit()
